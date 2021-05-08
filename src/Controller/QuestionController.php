29bb4cd1b48013c9304eda75c59e738a770a1f9e
AMARJIT SINGH <amarjitsingh52922@gmail.com>
2021-05-09 01:54:36 +0530

    two page added

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,12 +2,26 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
 
+    /**
+     * @Route("/",name="homepage")
+     */
     public function homepage()
     {
-        return new Response("great first page set up finally!!!");
+        return new Response("great first page set up finally dude!!!");
     }
+
+    /**
+     * @Route("/questions/{slug}",name="qname")
+     */
+     public function show($slug)
+     {
+        return new Response(sprintf(
+            'Question asked , %s!',$slug
+        ));
+     }
 }
